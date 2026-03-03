64b64deafb261c9185580adef43778586e51f636
manuelleon225 <manueljhoanleonmantilla@gmail.com>
2026-03-02 20:11:35 -0500

    Initial commit - Creación base del proyecto Spring Boot estudiantes

---
+++ src/test/java/com/sena/estudiantes/EstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
