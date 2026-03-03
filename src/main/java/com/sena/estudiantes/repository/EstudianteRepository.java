8893c00d74ddb0e6e1e640f359a7ba79fb9c0798
manuelleon225 <manueljhoanleonmantilla@gmail.com>
2026-03-02 20:41:01 -0500

    Creación EstudianteRepository con JpaRepository

---
+++ src/main/java/com/sena/estudiantes/repository/EstudianteRepository.java
@@ -0,0 +1,10 @@
+package com.sena.estudiantes.repository;
+
+import com.sena.estudiantes.model.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
+
+}
